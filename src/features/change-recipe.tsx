b3c615953db08777df8082e0b8126707c206dee2
GordeevAnt <antongordeev6721@gmail.com>
2025-06-01 13:03:52 +0800

    Deleted unused files

--- src/features/change-recipe.tsx
+++
@@ -1,9 +0,0 @@
-import PageTitle from "../shared/page-title";
-
-export default function ChangeRecipe () {
-    return(
-        <div>
-            <PageTitle title="Изменение рецепта"/>
-        </div>
-    )
-}
