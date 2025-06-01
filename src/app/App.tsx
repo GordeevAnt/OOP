6a70e09089db3e636eb571e855329f98cbb4cb68
GordeevAnt <antongordeev6721@gmail.com>
2025-06-01 06:14:16 +0200

    Updated Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,22 +3,22 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+<<<<<<< HEAD
 
 import { useEffect, useState } from 'react';
 import { RecipeType } from '../entities/data';
+=======
+>>>>>>> baccfc3 (Updated Routes)
 import AddRecipePage from '../pages/new-recipe-adding-page/new-recipe-adding-page';
-import { CategoryList, routes } from '../widgets/category-navi/category-navi';
+import { CategoryList } from '../widgets/category-navi/category-navi';
 import RecipePage from '../pages/recipe-page/recipe-page';
-import SidePanel from '../widgets/sidebar/side-panel';
-import SearchBar from '../widgets/search-bar/search-bar';
 import './styles.css'
-import { HomeIcon } from '../widgets/home-icon/home-icon';
 import RandomRecipePage from '../pages/random-recipe-page/random-recipe-page';
 import EditRecipePage from '../widgets/edit-recipe-form/edit-recipe-page';
 import FavoritesPage from "../pages/favorites-page/favorites-page";
-import RecipeFilter from '../widgets/recipe-filter/recipe-filter';
-import { RecipesProvider, useRecipes } from './recipes-context';
+import { RecipesProvider } from './recipes-context';
 import NotFoundPage from '../pages/NotFoundPage';
+<<<<<<< HEAD
 
 function AppContent() {
   const [filteredRecipes, setFilteredRecipes] = useState<RecipeType[]>([]);
@@ -52,20 +52,27 @@ function AppContent() {
           </ul>
         </nav>
       </header>
+=======
+import { Header } from './header';
+import CategoryPage from '../pages/category-page/category-page';
+
+function AppContent() {
+  return (
+    <>
+      <Header/>
+>>>>>>> baccfc3 (Updated Routes)
       <main>
         <Routes>
           <Route path="/" element={<CategoryList />}>
-            {routes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
+            <Route index element={<CategoryPage />} />
+            <Route path=":category" element={<CategoryPage />} />
           </Route>
+          <Route path=":category/recipe/:id" element={<RecipePage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/recipe/:id" element={<RecipePage />} />
           <Route path="/edit-recipe/:id" element={<EditRecipePage />} />
-          <Route path='/:categoryId/recipe/:id' element={<RecipePage />} />
-          <Route path='/newRecipe' element={<AddRecipePage />} />
-          <Route path='/randomRecipe' element={<RandomRecipePage />} />
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="/new-recipe" element={<AddRecipePage />} />
+          <Route path="/random-recipe" element={<RandomRecipePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
     </Router>
